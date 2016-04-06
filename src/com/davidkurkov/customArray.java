2face72ca9125713507bd357db2cacd289fe52b5
goishin <robb.nicholas@offerupnow.com>
2016-04-06 15:23:54 -0700

    Update customArray.java

--- src/com/davidkurkov/customArray.java
+++ src/com/davidkurkov/customArray.java
@@ -7,7 +7,8 @@ package com.davidkurkov;
 class CustomArray implements list{
     int[] myArray;
     int[] inputArray;
-
+    # Size should be a variable in here. 
+    # Every time you add one, increment the size. Every time you delete one, decrement it.
     int memorySpots = 10;
     int increaseMemoryBy = 3;
     int memoryBuffer = 3;
@@ -29,6 +30,11 @@ class CustomArray implements list{
     @Override
     public void remove(int value) {
         int index = findIndexOfValue(value);
+        # Once you found the spot, move all items to the right of it to the left one space.
+        # You gotta do this one. You gotta feel the pain of working with arrays.
+        # FEEL THE PAIN!!!!!
+        # FEEL IT!!!!
+        # We'll do more stupid array tricks after you finish the calculator
         if (index != -1) {
             myArray[index] = 0;
             inputArray[index] = 0;
@@ -37,6 +43,7 @@ class CustomArray implements list{
 
     @Override
     public int size() {
+        # This function should just be returning whatever value your size variable is storing.
         int count = 0;
         for (int i=0; i < inputArray.length; i++) {
             if (inputArray[i] != 0) {
@@ -78,6 +85,7 @@ class CustomArray implements list{
     }
 
     private int findEmptyIndex() {
+        # If you store your max position index as a variable instead of using a separate array, then you should never need this function
         int emptyIndex = -1;
         for (int i=0; i < inputArray.length; i++) {
             if (inputArray[i] == 0) {
@@ -117,7 +125,9 @@ class CustomArray implements list{
             int newLength = array.length * increaseMemoryBy;
             int[] tempArray = new int[newLength];
             for (int i=0; i < array.length; i++) {
-                if (array[i] != 0) {
+                if (array[i] != 0) { 
+                    # Don't check if it's empty, just overwrite. 
+                    # Checking is slower than assignment in all cases with base data types (int, bool, char, etc. (NOT including strings!))
                     tempArray[tempArrayIndex] = array[i];
                     tempArrayIndex += 1;
                 }
